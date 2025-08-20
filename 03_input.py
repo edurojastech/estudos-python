1e1ab84c4e2efa9e336e4071f2668a1b59c1d8f6
Eduardo Rojas <edurojas.developer@gmail.com>
2025-08-20 16:16:20 -0300

    feat: aula 03 de inputs

---
+++ 03_input.py
@@ -0,0 +1,11 @@
+# Utilizando inputs
+nome = input('Diga seu nome: \n')
+idade = int(input('Diga sua idade: \n'))
+cidadeResidencial = input('Diga sua cidade: \n')
+salario = float(input('Diga sua renda mensal: \n'))
+
+print("\n=== Dados ===")
+print('Seu nome:', nome)
+print('Sua idade:', idade)
+print('Cidade que mora:', cidadeResidencial)
+print(f'Seu salário atual: {salario}')
