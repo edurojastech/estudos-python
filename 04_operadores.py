360b6a07b5c5e1077bb13feb64f46b20b6cd8ff6
Eduardo Rojas <edurojas.developer@gmail.com>
2025-09-18 14:12:50 -0300

    aprendendo uso de operadores matematico com python

---
+++ 04_operadores.py
@@ -0,0 +1,15 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite um número: '))
+
+# operações math
+soma = n1 + n2
+subt = n1 - n2
+mult = n1 * n2
+divs = n1 / n2
+mod = n1 % n2
+
+print(soma)
+print(subt)
+print(mult)
+print(divs)
+print(mod)
