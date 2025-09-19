7c01d85fc09169195a5ec440dade66e504c277a0
Eduardo Rojas <edurojas.developer@gmail.com>
2025-09-19 18:11:13 -0300

    up

--- 04_operadores.py
+++ 04_operadores.py
@@ -1,12 +1,27 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite um número: '))
 
-# operações math
+# uso de operadores aritméticos
 soma = n1 + n2
 subt = n1 - n2
 mult = n1 * n2
 divs = n1 / n2
 mod = n1 % n2
+
+# uso de operadores de comparação ou relacional
+maiorq = n1 > n2
+menorq = n1 < n2
+iguais = n1 == n2
+diferent = n1 != n2
+diferentMaiorq = n1 >= n2
+diferentMenorq = n2 <= n2
+
+# uso de operadores de atribuição
+item = 'item do pec'
+somar = soma
+somar += 1  # somar = somar + 1
+subt -= 1 # subt = somar + 1
+mult *= 1
 
 print(soma)
 print(subt)
