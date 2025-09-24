4fce7e3e24fe5cc611a7fd56f3fd209b6245f483
Eduardo Rojas <edurojas.developer@gmail.com>
2025-09-24 12:56:52 -0300

    up nos estudos

---
+++ exercicios/antecessor_sucessor.py
@@ -0,0 +1,7 @@
+# programa que mostra o antecessor e sucessor dele
+numero = int(input('digite um numero: '))
+antecessor = numero - 1
+sucessor = numero + 1
+
+print(f'O antecessor do número: {numero} é {antecessor}')
+print(f'O sucessor do número: {numero} é {sucessor}')
